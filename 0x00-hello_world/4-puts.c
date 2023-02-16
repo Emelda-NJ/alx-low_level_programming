24846e74f648abbf17a97f4360a1b88ccc014635
Emelda-NJ <ezugwuemelda@gmail.com>
2023-02-16 15:19:14 -0800

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - The main() function is the entry point to a program.
+ *
+ * Return: This terminates the main() function and returns the value 0.
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle);
+	return (0);
+}
