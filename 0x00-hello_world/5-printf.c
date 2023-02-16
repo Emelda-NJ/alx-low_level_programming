b7c85db7919d9ba508c734ecc2365a62569b6b0c
Emelda-NJ <ezugwuemelda@gmail.com>
2023-02-16 15:36:26 -0800

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - The main() function is the entry point to a program.
+ *
+ * Return: This terminates the main() function and returns the value 0.
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
