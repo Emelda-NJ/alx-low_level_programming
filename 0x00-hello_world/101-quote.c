e5911e83490324ac05384b4ea40fb82be7bc1647
Emelda-NJ <ezugwuemelda@gmail.com>
2023-02-16 17:00:25 -0800

    UNIX is basically a simple operating system, but you have to be a genius to understand the simplicity

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - The main() function is the entry point to a program.
+ *
+ * Return: terminates the main() function and returns the value 1 to mean successful
+ */
+
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	return (1);
+}
