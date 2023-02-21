3469b19772420ecf1e64711dd5e6997a17fceb60
Emelda-NJ <ezugwuemelda@gmail.com>
2023-02-21 11:29:30 -0800

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+
+/**
+* main - This is the entry point to a program.
+* _putchar: writes a single character to the standard output stream, stdout
+* Return: returns the value 0 to mean successful
+*/
+
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
